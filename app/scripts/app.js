a65edc0d3060cf0fe24ccf7ed9a211acd9d26343
tmtrbocanegra <tmtrbocanegra@SGGT-PC.TMT.LOCAL>
2018-12-19 10:37:32 -0500

    Inicio

---
+++ app/scripts/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name opticaSbAdminFrontendApp
+ * @description
+ * # opticaSbAdminFrontendApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('opticaSbAdminFrontendApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngTouch'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name opticaSbAdminFrontendApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the opticaSbAdminFrontendApp
+ */
+angular.module('opticaSbAdminFrontendApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
